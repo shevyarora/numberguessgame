10ff8d40c50988038b77a5b08de55b763ea760ff
shevyarora <shevykumar26@gmail.com>
2023-10-06 10:10:54 +0530

    Update randomno.java

--- randomno.java
+++ randomno.java
@@ -1,43 +1,41 @@
-import java.util.*;
+import java.util.Random;
+import java.util.Scanner;
 import java.io.*;
-public class randomno {
-    
-    public static void main(String[] args) {
+public class randomno{
 
-        Scanner sc=new Scanner(System.in);
-
-        Random obj=new Random();
+       public static void main(String[] args) {
+       Scanner sc=new Scanner(System.in);  
+       Random obj = new Random();
+       System.out.println("Welcome to the Number guessing game !");
+       System.out.println("You have only 5 attempts !");
+        int numran=obj.nextInt(100-1+1)+1;
+        // System.out.println(numran);
         int guess;
-        int count=4;
-        int randomNum= obj.nextInt(100-1+1)+1;
         int flag=0;
-        System.out.println("**********WELCOME TO NUMBER GUESSING GAME **************");
-        // System.out.println(randomNum);
-        do{
+        int attempts=1;
+        while(attempts<=5){
 
-            System.out.println("Guess a number (1-100) :");
-            guess=sc.nextInt();
-            
-           
-            if(guess < randomNum){
-                System.out.println(" Higher number please !");
-            }
-            else if(guess > randomNum){
-                System.out.println(" Lower number please !");
-            }
-            else if(guess==randomNum){
-                System.out.println(" Congrats! you got the number .");
-                System.out.println("The Number was "+ randomNum);
+        System.out.println("Guess a number from (1-100) :");
+        guess=sc.nextInt();
+            if(guess==numran){
+                System.out.println("Congrats ! you got the number in "+attempts+" attempts !");
                 flag=1;
                 break;
             }
-            
-            count--;
-        }while(count>=0);
+            else if(guess < numran){
+                System.out.println("Higher number please ! "+ (5-attempts)+" attempts left!");
+            }
+            else{
 
-        if(flag==0){
-            System.out.println("You Lose !");
+                System.out.println("Lower number please ! "+ (5-attempts)+" attempts left !");
+            }
+            attempts++;
         }
 
+        if(flag==0){
+            System.out.println("You lose !");
+            System.out.println("The Number was "+ numran);
+        }
     }
+
 }
